71e0051dc1a0b02b8f7ec989755ae4c34b323da5
glbdmn <gabrieledamiani@hotmail.it>
2023-05-02 16:19:07 +0200

    esercizio completato

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+ export default{
+    name: "AppTitle",
+
+ }
+
+
+</script>
+
+<template>
+<div>
+  <h1>La mia prima app con vite</h1>
+ 
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
